03be549ae1807da2ef4602ed2a12aa24785e0eab
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-04-01 21:27:57 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -9,10 +9,10 @@ private:
   int top;
 public:
   TStack() : top(-1) {}
-  bool isEmpty() {
+  bool isEmpty() const {
     return top == -1;
   }
-  bool isFull() {
+  bool isFull() const {
     return size == top + 1;
   }
   T get() {
