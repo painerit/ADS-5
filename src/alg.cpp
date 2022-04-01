f339fbde68d8f65b42be87de61395a3519b09ea6
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-04-02 01:00:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,9 +3,57 @@
 #include <map>
 #include "tstack.h"
 
+int priority(char symb) {
+  if (symb == '(') {
+    return 0;
+  }
+  if (symb == ')') {
+    return 1;
+  }
+  if (symb == '+' || symb == '-') {
+    return 2;
+  }
+  if (symb == '/' || symb == '*') {
+    return 3;
+    }
+    return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  TStack <char, 100> myStack;
+  std::string pstfx;
+  for (int i = 0; i < inf.length(); ++i) {
+    if (priority(inf[i]) == -1) {
+      if (!pstfx.empty() && priority(inf[i - 1]) != -1) {
+        pstfx.push_back(' ');
+      }
+      pstfx.push_back(inf[i]);
+      } else if (priority(inf[i]) == 0 || priority(inf[i]) > priority(myStack.get()) || myStack.isEmpty()) {
+        myStack.push(inf[i]);
+      } else {
+        if (priority(inf[i]) == 1) {
+          while (myStack.get() != '(') {
+            pstfx.push_back(' ');
+            pstfx.push_back(myStack.get());
+            myStack.pop();
+          }
+          myStack.pop();
+        } else {
+          while (priority(myStack.get()) >= priority(inf[i])) {
+            pstfx.push_back(' ');
+            pstfx.push_back(myStack.get());
+            myStack.pop();
+          }
+          myStack.push(inf[i]);
+        }
+      }
+  }
+  while (!myStack.isEmpty()) {
+    pstfx.push_back(' ');
+    pstfx.push_back(myStack.get());
+    myStack.pop();
+  }
+  return pstfx;
 }
 
 int eval(std::string pref) {
