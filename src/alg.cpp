449ade34e315111f2606c726ea05e6bb9064311f
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-04-02 01:19:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,15 +23,16 @@ std::string infx2pstfx(std::string inf) {
   TStack <char, 100> myStack;
   std::string pstfx;
   for (int i = 0; i < inf.length(); ++i) {
-    if (priority(inf[i]) == -1) {
+    int prior = priority(inf[i]);
+    if (prior == -1) {
       if (!pstfx.empty() && priority(inf[i - 1]) != -1) {
         pstfx.push_back(' ');
       }
       pstfx.push_back(inf[i]);
-      } else if (priority(inf[i]) == 0 || priority(inf[i]) > priority(myStack.get()) || myStack.isEmpty()) {
+      } else if (prior == 0 || prior > priority(myStack.get()) || myStack.isEmpty()) {
         myStack.push(inf[i]);
       } else {
-        if (priority(inf[i]) == 1) {
+        if (prior == 1) {
           while (myStack.get() != '(') {
             pstfx.push_back(' ');
             pstfx.push_back(myStack.get());
@@ -39,7 +40,7 @@ std::string infx2pstfx(std::string inf) {
           }
           myStack.pop();
         } else {
-          while (priority(myStack.get()) >= priority(inf[i])) {
+          while (priority(myStack.get()) >= prior) {
             pstfx.push_back(' ');
             pstfx.push_back(myStack.get());
             myStack.pop();
